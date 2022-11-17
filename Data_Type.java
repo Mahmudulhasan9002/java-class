af5dd245d3f63f01399a976c601cf35834d6cde3
harry <mahmudulhasan4849002@gmail.com>
2022-11-16 22:15:13 -0500

    my java project

---
+++ Data_Type.java
@@ -0,0 +1,47 @@
+
+public class Data_Type {
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int g= 100;
+		System.out.println(g>50 && g<200);
+		
+		int j = 10;
+		int i = 20;
+		
+		if(i>5 && j<15) {
+			System.out.println("this is true ");
+		}else {
+			System.out.println("or its flass ");
+		}
+		
+		//System.out.println(i<100 && i<15);
+		
+		System.out.println(i<5 && i<15);
+		
+		if(20>15 && 10<20) {
+			System.out.println("troue ");
+		}
+		
+		System.out.println(10<5 || 10>9);
+		
+		System.out.println(i>10 || j<20);
+		System.out.println(!(5<4));
+		
+		int t=5;
+		int r= 6;
+		
+		System.out.println(!(5>6));
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ lerning_Static.java
@@ -0,0 +1,14 @@
+
+public class lerning_Static {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+	}
+
+}
